c4ed5e805c939d33fd1ecbf200cd1ac11c5ca42e
Alex Borelli <alex.sander.borelli@outlook.com.br>
2021-07-14 10:04:08 -0300

    :construction: Create BoardGame Object without CSS

---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,7 @@
+function PlayerName(player_number) {
+  return`
+    <h3>Player ${player_number}</h3>
+  `
+}
+
+export default PlayerName;
---
+++ src/objects/BoardGame/index.js
@@ -0,0 +1,11 @@
+import CardGame from '../../components/CardGame/index';
+
+function BoardGame(amountCards){
+  const $htmlCardGame = CardGame();
+
+  const $htmlBoardGame = $htmlCardGame.repeat(amountCards)
+
+  return $htmlBoardGame
+}
+
+export default BoardGame;
