0e5d8c86b00aeb398168d44afb5293c6a7be07c6
Alex Borelli <alex.sander.borelli@outlook.com.br>
2021-07-14 17:23:00 -0300

    :sparkles: Add Flip CardGame

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -4,8 +4,12 @@ import CardGame from '../CardGame/index';
 function CardFrontBack() {
   return /*html*/`
     <article class="card-front-back">
-      ${CardGame()}
-      ${CardGame("logo-javascript", "Logo do Javascript")}
+      <div class="card -front">
+        ${CardGame()}
+      </div>
+      <div class="card -back">
+        ${CardGame("logo-javascript", "Logo do Javascript")}
+      </div>
     </article>  
   `;
 }
